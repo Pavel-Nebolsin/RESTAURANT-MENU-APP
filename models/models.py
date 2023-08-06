f1061e13b2f0a9281c694cb4509766b82125195f
Pavel <ldrimanez@gmail.com>
2023-08-06 16:03:05 +0300

    minor fix

--- models/models.py
+++ models/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from sqlalchemy import Column, ForeignKey, Numeric, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy.orm import DeclarativeBase, Mapped, relationship
 
 
 class Base(DeclarativeBase):
@@ -10,31 +10,31 @@ class Base(DeclarativeBase):
 
 
 class Menu(Base):
-    __tablename__: str = 'menu'
+    __tablename__ = 'menu'
 
-    id: uuid.UUID = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    title: str = Column(String, unique=True, nullable=False)
-    description: str = Column(String, nullable=False)
-    submenus: list['SubMenu'] = relationship('SubMenu', back_populates='menu', cascade='all, delete')
+    id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    title: Mapped[str] = Column(String, unique=True, nullable=False)
+    description: Mapped[str] = Column(String, nullable=False)
+    submenus: Mapped[list['SubMenu']] = relationship('SubMenu', back_populates='menu', cascade='all, delete')
 
 
 class SubMenu(Base):
-    __tablename__: str = 'submenu'
+    __tablename__ = 'submenu'
 
-    id: uuid.UUID = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    title: str = Column(String, unique=True, nullable=False)
-    description: str = Column(String, nullable=False)
-    menu_id: uuid.UUID = Column(UUID, ForeignKey('menu.id'))
-    menu: 'Menu' = relationship('Menu', back_populates='submenus')
-    dishes: list['Dish'] = relationship('Dish', back_populates='submenu', cascade='all, delete')
+    id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    title: Mapped[str] = Column(String, unique=True, nullable=False)
+    description: Mapped[str] = Column(String, nullable=False)
+    menu_id: Mapped[uuid.UUID] = Column(UUID, ForeignKey('menu.id'))
+    menu: Mapped['Menu'] = relationship('Menu', back_populates='submenus')
+    dishes: Mapped[list['Dish']] = relationship('Dish', back_populates='submenu', cascade='all, delete')
 
 
 class Dish(Base):
-    __tablename__: str = 'dish'
+    __tablename__ = 'dish'
 
-    id: uuid.UUID = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    title: str = Column(String, unique=True, nullable=False)
-    description: str = Column(String, nullable=False)
-    price: float = Column(Numeric(precision=10, scale=2))
-    submenu_id: uuid.UUID = Column(UUID, ForeignKey('submenu.id'))
-    submenu: 'SubMenu' = relationship('SubMenu', back_populates='dishes')
+    id: Mapped[uuid.UUID] = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    title: Mapped[str] = Column(String, unique=True, nullable=False)
+    description: Mapped[str] = Column(String, nullable=False)
+    price: Mapped[float] = Column(Numeric(precision=10, scale=2))
+    submenu_id: Mapped[uuid.UUID] = Column(UUID, ForeignKey('submenu.id'))
+    submenu: Mapped['SubMenu'] = relationship('SubMenu', back_populates='dishes')
